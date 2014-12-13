168dca5705f1a771ed550fb61dd0c89826117528
ChrisThoma <cthoma@vt.edu>
2014-12-13 14:07:18 -0500

    Created the empty MonkeypodAndroidClientCodegen.

---
+++ src/main/java/com/wordnik/swagger/codegen/languages/MonkeypodAndroidClientCodegen.java
@@ -0,0 +1,9 @@
+package com.wordnik.swagger.codegen.languages;
+
+/**
+ * Created by christhoma on 12/13/14.
+ */
+public class MonkeypodAndroidClientCodegen extends AndroidClientCodegen {
+
+    
+}
